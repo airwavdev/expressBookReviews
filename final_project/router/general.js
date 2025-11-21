ca135c0ad1a8116618db809c99b74e9fa4d0e05b
Airwav <19alomar@gmail.com>
2025-11-21 19:10:57 +0000

    Add axios dependency and implement promise-based routes for book retrieval

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,42 +1,42 @@
 const express = require('express')
+const axios = require('axios')
 let books = require('./booksdb.js')
 let isValid = require('./auth_users.js').isValid
 let users = require('./auth_users.js').users
 const public_users = express.Router()
 
-// Register a new user (public endpoint)
+// ------------------------
+// Register a new user
+// ------------------------
 public_users.post('/register', (req, res) => {
   const { username, password } = req.body
 
-  // validate input
   if (!username || !password) {
     return res
       .status(400)
       .json({ message: 'Username and password are required' })
   }
 
-  // check if username is already taken
   if (!isValid(username)) {
     return res.status(400).json({ message: 'Username already exists' })
   }
 
-  // add user to shared users array
   users.push({ username: username, password: password })
-
   return res.status(201).json({ message: 'User registered successfully' })
 })
 
-// Get the book list available in the shop
-public_users.get('/', function (req, res) {
-  // Send all books as nicely formatted JSON
+// ------------------------
+// Task 1: Get all books
+// ------------------------
+public_users.get('/', (req, res) => {
   return res.send(JSON.stringify(books, null, 4))
 })
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
+// ------------------------
+// Task 2: Get book by ISBN
+// ------------------------
+public_users.get('/isbn/:isbn', (req, res) => {
   const isbn = req.params.isbn
-
-  // The books keys in booksdb.js are numeric keys 1..10 - treat isbn as string/number
   if (books[isbn]) {
     return res.send(books[isbn])
   } else {
@@ -46,8 +46,10 @@ public_users.get('/isbn/:isbn', function (req, res) {
   }
 })
 
-// Get book details based on author
-public_users.get('/author/:author', function (req, res) {
+// ------------------------
+// Task 3: Get book by author
+// ------------------------
+public_users.get('/author/:author', (req, res) => {
   const author = req.params.author
   const keys = Object.keys(books)
   const matched = []
@@ -67,8 +69,10 @@ public_users.get('/author/:author', function (req, res) {
   }
 })
 
-// Get all books based on title
-public_users.get('/title/:title', function (req, res) {
+// ------------------------
+// Task 4: Get book by title
+// ------------------------
+public_users.get('/title/:title', (req, res) => {
   const title = req.params.title
   const keys = Object.keys(books)
   const matched = []
@@ -88,8 +92,10 @@ public_users.get('/title/:title', function (req, res) {
   }
 })
 
-// Get book review
-public_users.get('/review/:isbn', function (req, res) {
+// ------------------------
+// Get book reviews
+// ------------------------
+public_users.get('/review/:isbn', (req, res) => {
   const isbn = req.params.isbn
   if (books[isbn]) {
     return res.send(books[isbn].reviews)
@@ -98,6 +104,61 @@ public_users.get('/review/:isbn', function (req, res) {
       .status(404)
       .json({ message: 'Book not found for the given ISBN' })
   }
+})
+
+// ------------------------
+// Tasks 10-13: Using Promises (with Axios)
+// ------------------------
+
+// Task 10: Get all books (Promise)
+public_users.get('/promises/books', (req, res) => {
+  axios
+    .get('http://localhost:5000/')
+    .then(response => res.json(response.data))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ message: 'Error fetching books', error: err.message })
+    )
+})
+
+// Task 11: Get book by ISBN (Promise)
+public_users.get('/promises/isbn/:isbn', (req, res) => {
+  const isbn = req.params.isbn
+  axios
+    .get(`http://localhost:5000/isbn/${isbn}`)
+    .then(response => res.json(response.data))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ message: 'Error fetching ISBN', error: err.message })
+    )
+})
+
+// Task 12: Get book by author (Promise)
+public_users.get('/promises/author/:author', (req, res) => {
+  const author = req.params.author
+  axios
+    .get(`http://localhost:5000/author/${author}`)
+    .then(response => res.json(response.data))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ message: 'Error fetching author', error: err.message })
+    )
+})
+
+// Task 13: Get book by title (Promise)
+public_users.get('/promises/title/:title', (req, res) => {
+  const title = req.params.title
+  axios
+    .get(`http://localhost:5000/title/${title}`)
+    .then(response => res.json(response.data))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ message: 'Error fetching title', error: err.message })
+    )
 })
 
 module.exports.general = public_users
